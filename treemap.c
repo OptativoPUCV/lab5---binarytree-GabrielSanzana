4fb7f207365ec43c8598a3a1170f13e5a5fd38df
Replit user <>
2023-04-20 19:27:42 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -82,10 +82,12 @@ TreeNode * minimum(TreeNode * x){
 
 void removeNode(TreeMap * tree, TreeNode* node) {
   if(node->left == NULL && node->right = NULL)
+  {
     if(node->parent->left == node)
       node->parent->left=NULL;
     else
       node->parent->right=NULL;
+  }
 }
 
 void eraseTreeMap(TreeMap * tree, void* key){
