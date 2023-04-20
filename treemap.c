f9b6f27175ce808c46882e577b75d3ea3ecf41cc
Replit user <>
2023-04-20 18:49:59 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -56,6 +56,7 @@ void insertTreeMap(TreeMap * tree, void* key, void * value) {
     auxParent = aux;
     if (tree->lower_than(aux->pair->key, key) == 1)
     {
+      printf("%d %d\n",aux->pair->key, key);
       aux = aux->right;
     }
     else
